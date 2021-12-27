bb6550252c7e400339f2150485d4ffdf5bee6e0c
ömer <omerisk4175@gmail.com>
2021-12-27 10:26:34 +0300

    ömer

--- araba.py
+++ araba.py
@@ -3,8 +3,6 @@ class Sofor:
     def __init__(self,marka,model):
 
      self.marka = marka 
-     self.smodel=model
-
+     self.model=model
      
-   
 
---
+++ surucu.py
@@ -0,0 +1,5 @@
+ class Araba:
+       def __init__(self,marka,model,surucu):
+        self.marka = marka
+        self.model=model
+        self.surucu=Sofor 
